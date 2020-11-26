2b3ac1da7b6e918ea08d217a029f0cba03bec382
dominyque869 <72576271+dominyque869@users.noreply.github.com>
2020-11-26 18:09:17 +0200

    Add files via upload

--- main4.py
+++ main4.py
@@ -10,10 +10,14 @@ for output in decoded_tx['vout']:
 print("Outputs: ", tx_value)
 input_tx_value = 0
 for input in decoded_tx['vin']:
-        input_raw_tx = p.getrawtransaction(input['txid'])
-        input_decoded_tx = p.decoderawtransaction(input_raw_tx)
-        for output in input_decoded_tx['vout']:
-            input_tx_value = input_tx_value + output['value']
+        if input.has_key("'txid'"):
+            input_tx = input['txid']
+            input_raw_tx = p.getrawtransaction(input_tx)
+            input_decoded_tx = p.decoderawtransaction(input_raw_tx)
+            for output in input_decoded_tx['vout']:
+                input_tx_value = input_tx_value + output['value']
+            fee = input_tx_value - tx_value
+        else:
+            fee = 0
 print("Inputs: ", input_tx_value)
-fee = input_tx_value - tx_value
 print ("Transaction fee: ", fee)
